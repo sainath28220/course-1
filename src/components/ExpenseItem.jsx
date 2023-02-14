10e38674b1d7afa68763d7e02700af949cc8eae8
sainath28220 <92886694+sainath28220@users.noreply.github.com>
2023-02-14 22:24:46 +0530

    2 files modified, 6 added

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = ({expenses}) => {
   const expenseRender = expenses.map(expense=>{
     return(
       <div key={expense.id}  className="expense-item">
-        <div>{expense.date.toLocaleString()}</div>
+        <ExpenseDate date={expense.date}/>
         <div className="expense-item__description">
           <h2>{expense.title}</h2>
           <div className="expense-item__price">{expense.amount} INR</div>
